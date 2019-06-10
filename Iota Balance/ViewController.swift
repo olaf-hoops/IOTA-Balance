b2e82eb80b53b5a4e456169ff1db39c9aaaf3d07
Olaf Hoops <olaf.hoops@gmail.com>
2019-06-10 15:16:49 +0200

    Initial Commit

---
+++ Iota Balance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Iota Balance
+//
+//  Created by onehitwonder on 10.06.19.
+//  Copyright © 2019 onehitwonder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
